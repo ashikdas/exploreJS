fae0d325a1b9b2962f0adf9df2b6950cde4207f7
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-29 17:06:40 +0600

    data type

---
+++ hello.js
@@ -0,0 +1,9 @@
+var bananaPrice = 12;
+console.log(typeof bananaPrice)
+
+var name = "Ashik";
+
+console.log(typeof name);
+
+var promise = "I promise that i will be a programmer.";
+console.log(promise.split(' '));
