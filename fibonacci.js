53750b1a18584420a874cab290bec27157ce1cb3
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-02 00:36:28 +0600

    fibonacci in recursive way

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,7 @@
 function fibonacci(n){
     var fibo = [0, 1];
 
-    for(var i=2; i<=10; i++){
+    for(var i=2; i<=n; i++){
         fibo[i] = fibo[i-1] + fibo[i-2];
         // console.log(fibo[i]);
     }
