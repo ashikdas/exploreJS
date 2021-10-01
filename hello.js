797c121ce3aa5d183df90eaf5242c7a0b31283ff
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-01 21:36:28 +0600

    inch to feet

--- hello.js
+++ hello.js
@@ -7,3 +7,6 @@ console.log(typeof name);
 
 var promise = "I promise that i will be a programmer.";
 console.log(promise.split(' '));
+
+var number1 = '25.5';
+number1 = parseFloat(number1);
---
+++ inchToFeet.js
@@ -0,0 +1,12 @@
+function inchToFeet(inch){
+    var feet = inch / 12;
+    return feet;
+}
+var senior = [156, 288, 300];
+
+var nanaFeet = inchToFeet(senior[0]);
+console.log(nanaFeet);
+var naniFeeet = inchToFeet(234);
+console.log(naniFeeet);
+var dadifeet = inchToFeet(500);
+console.log(dadifeet);
