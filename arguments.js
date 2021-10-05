0f30e505d58816ff0b20fc3066e62a0f7f88cdac
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-06 00:22:06 +0600

    arguments discussesed

---
+++ arguments.js
@@ -0,0 +1,14 @@
+function addNumbers(num1, num2){
+    console.log(arguments);
+    var sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const num = arguments[i];
+        sum = sum + num;
+        
+    }
+    return sum;
+
+}
+
+result = addNumbers(3, 5, 8, 15, 29);
+console.log(result);
