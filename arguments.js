2623765dd52b4c11a40f996cad15db0145293d4e
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-10-06 00:32:13 +0600

    inner function

--- arguments.js
+++ arguments.js
@@ -3,9 +3,13 @@ function addNumbers(num1, num2){
     var sum = 0;
     for (let i = 0; i < arguments.length; i++) {
         const num = arguments[i];
-        sum = sum + num;
-        
+        sum = sum + num;   
     }
+
+    function logInfo(message){
+        console.log(message);
+    }
+    logInfo('Good Morning!')
     return sum;
 
 }
